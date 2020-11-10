915fe77ac01d8265e3ed92bbaf73f6ed4e3c8002
mshajid <70450142+mshajid@users.noreply.github.com>
2020-11-10 17:21:02 +0530

    DOM

---
+++ script.js
@@ -0,0 +1,5 @@
+const form = document.getElementById("form");
+const form = document.getElementById("username");
+const form = document.getElementById("email");
+const form = document.getElementById("password");
+const form = document.getElementById("password2");
