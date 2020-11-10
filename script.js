b7652edfadbff7b014bd0c8589be735b70396dc0
mshajid <70450142+mshajid@users.noreply.github.com>
2020-11-10 17:50:22 +0530

    removed if statement method

--- script.js
+++ script.js
@@ -4,4 +4,32 @@ const email = document.getElementById("email");
 const password = document.getElementById("password");
 const password2 = document.getElementById("password2");
 
+// Show Input Error Message
+function showError(input, message) {
+    const formControl = input.parentElement;
+    formControl.className = "form-control error";
+    const small = formControl.querySelector("small");
+    small.innerText = message;
+}
+
+// Show Success Green Outline
+
+function showSuccess(input) {
+    const formControl = input.parentElement;
+    formControl.className = "form-control success";
+}
+
+// Check email is valid
+
+function isValidEmail(email) {
+    const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(String(email).toLowerCase());
+}
+
+// Event Listeners 
+form.addEventListener("submit", function(e) {
+    e.preventDefault();
+    
+
+});
 
